91bfb1e591c479219184c83d94e81b8e2ecd1bb0
Ana Clara <anaclaralima9999@gmail.com>
2026-05-15 12:30:18 -0300

    Atualização do index.php

--- GRUPO DO TCC/PW2/farmacia-vav/index.php
+++ GRUPO DO TCC/PW2/farmacia-vav/index.php
@@ -1,11 +1,36 @@
 <?php 
-  require 'conexao.php'; // Puxa o banco
-  require 'cabecalho.php'; // Puxa o topo visual
+  require_once "config/conexao.php";; // Puxa o banco
+  require_once 'includes/header.php'; // Puxa o topo visual
 ?>
 
 <h2>Bem-vindo à Farmácia ____</h2>
 <p>Aqui você pode gerenciar a Fármacia ___.</p>
-
 <?php 
-  require 'rodape.php'; // Puxa o final do site
+$sql = "SELECT * FROM produtos ORDER BY id ASC";
+$stmt = $conexao->prepare($sql);
+$stmt->execute();
+
+
+// 3. BUSCAR TODOS (fetchAll):
+// O PDO::FETCH_ASSOC organiza cada linha como um array associativo ['campo' => 'valor']
+$produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// 4. EXIBIR (O laço de repetição):
+echo "<h2>Lista de Contatos</h2>";
+
+if ($produtos) {
+    foreach ($produtos as $produto) {
+        echo "ID: " . $produto['id'] . " | ";
+        echo "Nome: " . $produto['nome'] . " | ";
+        echo "Fabricante: " . $produto['fabricante'] . "<br>";
+        echo "Preco: " . $produto['preco'] . "<br>";
+        echo "Estoque: " . $produto['estoque'] . "<br>";
+        echo "-----------------------------------<br>";
+    }
+} else {
+    echo "A agenda está vazia.";
+}
+  
+
+  require_once 'includes/footer.php'; // Puxa o final do site
 ?>
