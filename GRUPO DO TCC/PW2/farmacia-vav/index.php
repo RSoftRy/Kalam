cde9ed9cc01485bc3afcf3e871119959d5e82769
Ana Clara <anaclaralima9999@gmail.com>
2026-05-15 11:28:11 -0300

    Atualização de arquivo

---
+++ GRUPO DO TCC/PW2/farmacia-vav/index.php
@@ -0,0 +1,11 @@
+<?php 
+  require 'conexao.php'; // Puxa o banco
+  require 'cabecalho.php'; // Puxa o topo visual
+?>
+
+<h2>Bem-vindo à Farmácia ____</h2>
+<p>Aqui você pode gerenciar a Fármacia ___.</p>
+
+<?php 
+  require 'rodape.php'; // Puxa o final do site
+?>
