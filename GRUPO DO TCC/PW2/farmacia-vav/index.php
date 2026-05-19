0274dc22f05850dfe3aa99a8cfa43634e353862d
Ana Clara <anaclaralima9999@gmail.com>
2026-05-19 20:34:46 -0300

    Alteração do design do index pelo css

--- GRUPO DO TCC/PW2/farmacia-vav/index.php
+++ GRUPO DO TCC/PW2/farmacia-vav/index.php
@@ -3,8 +3,20 @@
   require_once 'includes/header.php'; // Puxa o topo visual
 ?>
 
-<h2>Bem-vindo à Farmácia VAV</h2> <br>
-<p>Aqui você pode gerenciar a Fármacia VAV.</p> <br> <br>
+<div class="espaco">
+<h2 style="text-align: center">Bem-vindo à Farmácia VAV</h2>
+<p style="text-align: center">Aqui você pode gerenciar a Fármacia VAV.</p> <br> <br>
+</div>
+<div style="padding:5px">
+  
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Fabricante</th>
+        <th>Preço</th>
+        <th>Estoque</th>
+    </tr>
 <?php 
 $sql = "SELECT * FROM produtos ORDER BY id ASC";
 $stmt = $conexao->prepare($sql);
@@ -17,19 +29,23 @@ $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // 4. EXIBIR (O laço de repetição):
 echo "<h2>Lista de Produtos</h2>";
+
 if ($produtos) {
     foreach ($produtos as $produto) {
-        echo "ID: " . $produto['id'] . " | ";
-        echo "Nome: " . $produto['nome'] . " | ";
-        echo "Fabricante: " . $produto['fabricante'] . "<br>";
-        echo "Preco: " . $produto['preco'] . "<br>";
-        echo "Estoque: " . $produto['estoque'] . "<br>";
-        echo "-----------------------------------<br>";
+        echo  "<td>" . $produto['id'] . "</td>";
+        echo  "<td>" . $produto['nome'] . "</td>";
+        echo  "<td>" . $produto['fabricante'] . "</td>";
+        echo  "<td>" . $produto['preco'] . "</td>";
+        echo  "<td>" . $produto['estoque'] . "</td>";
+        echo "<tr>";
     }
 } else {
-    echo "A agenda está vazia.";
+    echo "A lista está vazia.";
 }
+  ?>
+  </table>
+</div>
   
-
+<?php
   require_once 'includes/footer.php'; // Puxa o final do site
 ?>
